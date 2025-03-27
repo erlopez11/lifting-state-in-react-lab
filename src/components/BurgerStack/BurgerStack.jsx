e87f84a6da3761302670fbca2536cf861b62d161
erlopez11 <lopezelainy@gmail.com>
2025-03-26 21:26:19 -0500

    wip: display stack ingredients

--- src/components/BurgerStack/BurgerStack.jsx
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -2,7 +2,12 @@
 const BurgerStack = (props) => {
     return (
         <ul>
-
+            {props.stackIngredients.map((ingredient, index) => (
+                <li style={{backgroundColor: ingredient.color}} key={index}>
+                    <p>{ingredient.name}</p>
+                    <button onClick={() => props.removeFromBurger(ingredient)} type="submit">X</button>
+                </li>
+            ))}
         </ul>
     );
 };
