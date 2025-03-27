b5d63351a1c6988a512ad97e3fc53a26a42c41e9
erlopez11 <lopezelainy@gmail.com>
2025-03-26 21:27:32 -0500

    wip: add onClick to add ingredient to addToBurger fn in App

--- src/components/IngredientList/IngredientList.jsx
+++ src/components/IngredientList/IngredientList.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 
 const IngredientList = (props) => {
+
     return (
         <ul>
             {props.ingredients.map((ingredient, index) => (
                 <li style={{backgroundColor: ingredient.color}} key={index}>
                     <p>{ingredient.name}</p>
-                    <button>+</button>
+                    <button onClick={() => props.addToBurger(ingredient)} type="submit">+</button>
                 </li>
             ))}
         </ul>
